8760d839c65897a66fd59f93a6d73ea76a56db72
Sheomight <nikita-skop@yandex.ru>
2023-08-06 21:28:30 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   lintOnSave: false,
   filenameHashing: false,
-  publicPath: '/Atlas/'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Atlas/'
+    : '/'
 })
