789e7b7b3a62a7003d69de03894d9391a78a0849
Sheomight <nikita-skop@yandex.ru>
2023-08-06 21:19:18 +0300

    dist

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   lintOnSave: false,
-  filenameHashing: false
+  filenameHashing: false,
+  publicPath: '/Atlas/'
 })
